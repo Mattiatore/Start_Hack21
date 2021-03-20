ab1ae61b65112008b2db8cfb1ac7b7eb38c5d648
Mattia Mariantoni <47110871+Mattiatore@users.noreply.github.com>
2021-03-20 14:59:32 +0100

    Add files via upload
    vwap temporary

---
+++ Alerts/vwap.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 11:38:59 2021
+
+@author: Mattia
+"""
+
+
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+
+one_hour= pd.read_csv("143289.csv", names= ["Time", "Vwap_1"], skiprows=1)
+three_hour= pd.read_csv("143325.csv", names= ["Time", "Vwap_3"], skiprows=1)
+
+df = pd.merge(one_hour, three_hour, on='Time')
+
+#Naive comparison as baseline
+
+# compare vwap 1 hour with vwap 3 hours to predict if increasing or decreases in the next hour
+
+#True implies increasing in both Prediction and Occurence
+df["Prediction"]=(df.Vwap_1<df.Vwap_3).values
+
+temp=df.Vwap_1[1:]
+#print(temp)
+
+df.drop(df.tail(1).index,inplace=True)
+df["Occurence"]=(df.Vwap_1.values<temp).values
+
+df.fillna(df.mean(), inplace=True)
+
+print(confusion_matrix(df.Occurence, df.Prediction, normalize="true"))
+
+#Random forest
+
+y = df.Occurence
+X = df.drop(columns=["Time", "Prediction", "Occurence"])
+
+clf = GradientBoostingClassifier(n_estimators=75, learning_rate=1.0, max_depth=1, random_state=42).fit(X, y)
+
+clf.fit(X, y)
+
+print(confusion_matrix(y, clf.predict(X) , normalize="true"))
+
